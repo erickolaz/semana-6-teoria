565e5a6b6f0910057f0472802f0d0f1a4a135671
erick <erick.olazabal.morales@gmail.com>
2019-09-10 18:58:24 -0500

    dropb

---
+++ Controllers/pc1Controller.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace semana_6_teoria.Controllers
+{
+    public class pc1Controller:Controller
+    {
+         public IActionResult dropb ()
+        {
+            return View();
+        }
+    }
+}
